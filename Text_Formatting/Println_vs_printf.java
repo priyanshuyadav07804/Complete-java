9b4295323e705f47937a4ab0f6391d3fefdff689
priyanshu yadav <80442935+priyanshuyadav07804@users.noreply.github.com>
2022-03-10 01:27:23 +0530

    Create Println_vs_printf.java

---
+++ Text_Formatting/Println_vs_printf.java
@@ -0,0 +1,12 @@
+//println vs printf
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+// 		System.out.println("GfG!");
+        float a = 3.141f;
+        System.out.println(a);
+        System.out.printf("%f",a);
+	}
+}
