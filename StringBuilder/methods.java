9cce08ef4d5843a7037adff4d230b437efe081da
priyanshu yadav <80442935+priyanshuyadav07804@users.noreply.github.com>
2022-03-22 10:36:02 +0530

    Update methods.java

--- StringBuilder/methods.java
+++ StringBuilder/methods.java
@@ -1,9 +1,4 @@
-
-
-/*package whatever //do not write package name here */
-
 import java.io.*;
-
 class GFG {
 	public static void main (String[] args) {
 		StringBuilder sb = new StringBuilder("ganga");
