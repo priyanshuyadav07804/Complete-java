67b5ab77f9af2038390c4524f1078b712ae98831
priyanshu yadav <80442935+priyanshuyadav07804@users.noreply.github.com>
2022-04-18 16:55:14 +0530

    Create overriding_of_equals_method.java

---
+++ oops/overriding_of_equals_method.java
@@ -0,0 +1,33 @@
+class A{
+    int x;
+    A(int val){
+        x = val;
+    }
+    public boolean equals(Object obj){
+        if(this == obj){     //if both point to same object or not;
+            return true;
+        }
+        if(obj instanceof A){               //1st obj(of type Object) ko type A me bnana
+            A otherobj = (A) obj;           //hoga bcoz x , A ka field h naki object ka 
+            return this.x == otherobj.x;    //overall hme content compare krna h isliye
+        }else{               
+            return false;   //means kuch bhi same nhi h
+        }
+    }
+}
+public class test{
+    public static void main(String[] args){
+        A a1 = new A(4);     //dif
+        A a2 = new A(5);     //dif
+        A a3 = new A(4);     //dif
+        A a4 = a1;          //point to same object
+      
+        System.out.println(a1.equals(a2));   //false
+        System.out.println(a1.equals(a3));   //true
+        System.out.println(a1.equals(a4));   //true
+        
+        System.out.println(a1 == a2);        //false
+        System.out.println(a1 == a3);        //false
+        System.out.println(a1 == a4);        //true;
+    }
+}
