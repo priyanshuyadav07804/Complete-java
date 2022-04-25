838e99b8b57595013463ac561f40a14a613ee891
priyanshu yadav <80442935+priyanshuyadav07804@users.noreply.github.com>
2022-04-25 10:41:25 +0530

    Update overriding_of_equals_method.java

--- oops/overriding_of_equals_method.java
+++ oops/overriding_of_equals_method.java
@@ -1,12 +1,14 @@
+// Equals()=> only verifies if the references are same. It does not verify if the contents are same. Content verification 
+//            is the responsibility of the overriding implementation.
 class A{
     int x;
     A(int val){
         x = val;
     }
     public boolean equals(Object obj){
-        if(this == obj){     //if both point to same object or not;
-            return true;
-        }
+//         if(this == obj){     //if both point to same object or not;
+//             return true;
+//         }
         if(obj instanceof A){               //1st obj(of type Object) ko type A me bnana
             A otherobj = (A) obj;           //hoga bcoz x , A ka field h naki object ka 
             return this.x == otherobj.x;    //overall hme content compare krna h isliye
@@ -23,8 +25,8 @@ public class test{
         A a4 = a1;          //point to same object
       
         System.out.println(a1.equals(a2));   //false
-        System.out.println(a1.equals(a3));   //true
-        System.out.println(a1.equals(a4));   //true
+        System.out.println(a1.equals(a3));   //true bcoz content same h 
+        System.out.println(a1.equals(a4));   //true bcoz dono same addres ko point kr rhe h
         
         System.out.println(a1 == a2);        //false
         System.out.println(a1 == a3);        //false
