bd79374de72b9330e0c921e4d4ee72ae1515e2d3
priyanshu yadav <80442935+priyanshuyadav07804@users.noreply.github.com>
2022-04-29 12:27:15 +0530

    Update Car_MaxSpeed.java

--- Interface/Car_MaxSpeed.java
+++ Interface/Car_MaxSpeed.java
@@ -1,3 +1,15 @@
+
+/*Create an interface called Car with two abstract methods String getName(), int getMaxSpeed().one default method void applyBreak() with code snippet
+System.out.println("Applying break on " + getName());
+
+A static method "Car getFastestCar(Car car1, Car car2)" which returns car1 if the maxSpeed of car1 is greater than or equal to that of car2, else return car2.
+
+Create a class called BMW which implements the interface Car and provides the implementation for the abstract methods getName() and getMaxSpeed().
+create a class called Audi which implements the interface Car and provides the implementation for the abstract methods getName() and getMaxSpeed().
+
+Create a public class called MainApp with the main() method.
+Take the input from the command line arguments. Create objects for the classes BMW and Audi then print the fastest car.*/
+
 public class Test{
     public static void main(String[] args){
         BMW b = new BMW("BMW",200);
